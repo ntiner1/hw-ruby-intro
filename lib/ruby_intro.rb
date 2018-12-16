894d00841111019f1c1157dee8d4269903d6b92e
Nick <ntiner1@tamu.edu>
2018-12-16 22:40:31 +0000

    Finished part three

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -63,5 +63,16 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0 
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string()
+    sprintf("$%0.02f", price.to_f)
+  end
 end
