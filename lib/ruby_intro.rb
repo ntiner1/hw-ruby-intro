331ab974ec69b6860b0ce137ab98c87387101d19
Nick <ntiner1@tamu.edu>
2018-12-16 22:03:13 +0000

    Finished part one

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,42 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each do |num|
+    sum += num
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr.first if arr.size == 1
+    
+  max_lower = -1*Float::INFINITY
+  max_upper = -1*Float::INFINITY
+  arr.each do |num|
+    if num >= max_upper
+      max_lower = max_upper
+      max_upper = num
+    elsif num >= max_lower
+      max_lower = num
+    else
+      # do no swaps      
+    end
+  end
+  max_upper + max_lower
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.size < 2
+  for i in 0...(arr.size)
+    for j in (i+1)...(arr.size)
+      if (arr[i] + arr[j]) == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
